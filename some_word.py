332fd91ebaf76369d90b24bfa0cb18cedf49bdac
rishabh <jainrishabh880@gmail.com>
2016-05-26 14:38:31 +0530

    returns the sum of wordsin a text

---
+++ some_word.py
@@ -0,0 +1,32 @@
+import string
+
+
+def make_histogram(filename):
+	hist = {}
+	fin = open(filename)
+	for line in fin:
+		process_line(line, hist)
+		
+	return hist
+	
+	
+def process_line(line, hist):
+	line = line.replace('-', '')
+	for word in line.split():
+		word = word.strip(string.punctuation + string.whitespace)
+		word = word.lower()
+		hist[word] = hist.get(word,0) + 1
+		
+		
+def word_some(hist):
+	return sum(hist.values())
+	
+		
+		
+
+if __name__ == '__main__':
+			hist = make_histogram ('king.txt')
+			print word_some(hist)
+			
+	
+		
